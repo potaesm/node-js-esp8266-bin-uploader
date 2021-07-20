72541419380a283ca4909050c0cd5a023a52100d
Suthinan Musitmani <suthinan.musitmani@gmail.com>
2021-07-20 17:48:50 +0700

    Add other os support

--- index.js
+++ index.js
@@ -16,14 +16,23 @@ function run(command) {
 
 (async function main() {
     console.log('Getting port...');
-    const chgport = await run('chgport');
-    const deviceMapper = `${chgport}`.split('\n').filter(mapper => mapper.includes('Silab'))[0];
+    let listPortCommand = 'chgport';
+    let listFileCommand = 'dir';
+    let pythonType = '-win';
+    const pathSeparator = require('path').sep;
+    if (pathSeparator === '/') {
+        listPortCommand = 'ls /dev/cu.*';
+        listFileCommand = 'ls';
+        pythonType = '';
+    }
+    const portList = await run(listPortCommand);
+    const deviceMapper = `${portList}`.split('\n').filter(mapper => mapper.toLowerCase().includes('lab'))[0];
     if (!deviceMapper) {
         return console.log('Device not found');
     }
     const port = deviceMapper.split(' ')[0];
     console.log('Found port ', port);
-    const dir = await run('cd build && dir && cd ..');
+    const dir = await run(`cd build && ${listFileCommand} && cd ..`);
     const binFileDetail = `${dir}`.split('\n').filter(item => item.includes('.bin'))[0];
     if (!binFileDetail) {
         return console.log('Binary file not found');
@@ -32,6 +41,6 @@ function run(command) {
     const binFileName = binFileDetailArr[binFileDetailArr.length - 1];
     console.log('Found binary file ', binFileName);
     console.log('Start uploading...');
-    const result = await run(`python3\\python3 tools/upload.py --chip esp8266 --port ${port} --baud 115200 --before default_reset --after hard_reset write_flash 0x0 build\\${binFileName}`);
+    const result = await run(`python3${pythonType}${pathSeparator}python3 tools/upload.py --chip esp8266 --port ${port} --baud 115200 --before default_reset --after hard_reset write_flash 0x0 build${pathSeparator}${binFileName}`);
     return console.log(result);
 })();
